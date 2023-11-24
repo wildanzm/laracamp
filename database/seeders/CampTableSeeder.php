9ec2dd0f9de437ced48e6ffb576dbe81b3377e5f
wildanzm <wildanmanapi123@gmail.com>
2023-11-24 08:23:06 +0700

    Seeder

---
+++ database/seeders/CampTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Camp;
+
+class CampTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $camps = [
+            [
+                'title' => 'Giat Belajar',
+                'slug' => 'giat-belajar',
+                'price' => 280,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ],
+            [
+                'title' => 'Baru Belajar',
+                'slug' => 'baru-belajar',
+                'price' => 140,
+                'created_at' => date('Y-m-d H:i:s', time()),
+                'updated_at' => date('Y-m-d H:i:s', time())
+            ]
+        ];
+
+        Camp::insert($camps);
+    }
+}
